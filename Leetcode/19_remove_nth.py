3154e7ce901d4f1a298643eb84926f61ba639153
Barnali <barnalidatta@gmail.com>
2025-02-27 23:17:04 -0800

    changes

--- Leetcode/19_remove_nth.py
+++ Leetcode/19_remove_nth.py
@@ -1,4 +1,5 @@
 # 19- Remobe nth node from end of the list
+# Fast and slow pointer method
 
 class Node:
     def __init__(self, new_data):
@@ -7,8 +8,50 @@ class Node:
 
 def remove_nth_node(head, n):
     currentnode = head
-    while(currentnode.next):
+    # fast = currentnode.next # fast = 2
+    #slow = currentnode
+    
+    # if currentnode.next != None:
+    #     for _ in range(n):
+    #         if fast.next != None:
+    #             fast = fast.next
+    #             print(f" fast var after running {n} loop {fast.data}")
+        
+    #     # fast pointer pointing to null. This is to make 
+    #     # the slow pointer stop right before the node need to be removed.
+    #     while fast: 
+    #         fast, slow = fast.next, slow.next
+    #         print(f" slow var when fast reaching NULL {slow.data}")
+    #     if slow.next != None:
+    #         slow.next = slow.next.next
+    #     else:
+    #         print("here")
+    #         currentnode = slow
+    #         print(slow.data)
+    #         return slow
+    # else:
+    #     currentnode = currentnode.next
+    # return currentnode
+    ############################################################
+    fast = currentnode
+    slow = currentnode
+
+    if currentnode.next != None:
+        for _ in range(n):
+            fast = fast.next
+            # print(f" fast var after running {n} loop {fast.data}")
+        if not fast:
+            return currentnode.next
+        # fast pointer pointing to null. This is to make 
+        # the slow pointer stop right before the node need to be removed.
+        while fast.next: 
+            fast, slow = fast.next, slow.next
+            print(f" slow var when fast reaching NULL {slow.data}")
+            slow.next = slow.next.next
+    else:
         currentnode = currentnode.next
+    return currentnode
+
 
 def print_list(head):
     currentnode = head
@@ -19,10 +62,10 @@ def print_list(head):
 if __name__ == "__main__":
     head = Node(1)
     head.next = Node(2)
-    head.next.next = Node(3)
-    head.next.next.next = Node(4)
-    head.next.next.next.next = Node(5)
+    # head.next.next = Node(3)
+    # head.next.next.next = Node(4)
+    # head.next.next.next.next = Node(5)
 
-    print_list(head)
+    # print_list(head)
     remove_nth_node(head, n=2)
     print_list(head)
